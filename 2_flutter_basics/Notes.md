fb0599e72bf240bc351821ae0bc8ae3e2931cf73
JonShort <JonShort@users.noreply.github.com>
2024-04-30 21:36:16 +0100

    [new] - learning session 5

---
+++ 2_flutter_basics/Notes.md
@@ -0,0 +1,26 @@
+## Widgets
+
+### Container
+* Simplest widget
+* Can be styled with boxDecoration
+
+### Center
+* Aligns child to middle of parent
+
+### Text
+* Displays string
+* Can be styled with style
+
+### Column
+* Accepts children, lays them out in a vertical column
+* Key layout values
+  * mainAxisSize
+  * crossAxisAlignment
+  * mainAxisAlignment
+
+### SizedBox
+* Just a generic sizer - preferred in most cases over padding / margin as it's more readable
+
+### Padding
+* Utility for spacing
+* Reccomended to use only within components rather than to space them
--- 2_flutter_basics/lib/main.dart
+++ 2_flutter_basics/lib/main.dart
@@ -12,13 +12,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.amber)),
       // Scaffold is a basic layout
       home: Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: const Text('Flutter Basics'),
         ),
-        body: const Center(
-          child: Text('Hello World!'),
+        body: Container(
+          color: Colors.pink,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                color: Colors.green,
+                child: const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text('First column child'),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blue,
+                ),
+                child: const Center(
+                  child: Text(
+                    'Hello world!',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text('Last column child')
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => debugPrint('clicked'),
+          child: const Icon(Icons.ac_unit),
         ),
       ),
     );
